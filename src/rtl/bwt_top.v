16cd2b35a512325724ad36c977167d9c63330c9a
Angelina Kowalik <kowalik.angelina@gmail.com>
2020-05-06 15:07:35 +0200

    init vivado project

---
+++ src/rtl/bwt_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.05.2020 14:16:34
+// Design Name: 
+// Module Name: bwt_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bwt_top(
+
+    );
+endmodule
